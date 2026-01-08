deefef2c0442de1c3b31e51a023a6fcf7db7d665
Sourabh Patil <95458021+sourabh0396@users.noreply.github.com>
2026-01-08 09:37:56 +0530

    Revert "Add apply-to-job and get-all-applications APIs"
    This reverts commit d6e6d31915aec3228dbede72abb7c272a75ab337.

--- backend/src/routes/userRotes.ts
+++ backend/src/routes/userRotes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { isUserAuth } from "../middleware/UserAuth.js";
-import { addSkilltoUser, applyToJob, deleteSkillFromUser, getAllApplications, getUserProfile, MyProfile, updatedUserProfile, updateProfilePic, updateResume } from "../controller/userController.js";
+import { addSkilltoUser, deleteSkillFromUser, getUserProfile, MyProfile, updatedUserProfile, updateProfilePic, updateResume } from "../controller/userController.js";
 import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
@@ -11,6 +11,4 @@ router.put('/update/pic', isUserAuth, uploadFile, updateProfilePic);
 router.put('/update/resume', isUserAuth, uploadFile, updateResume);
 router.post('/skill/add', isUserAuth, addSkilltoUser);
 router.delete('/skill/delete', isUserAuth, deleteSkillFromUser);
-router.post('/apply/job', isUserAuth, applyToJob);
-router.get('/applications/all', isUserAuth, getAllApplications);
 export default router;
