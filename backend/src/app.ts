2dd181d351f81805ded89c8110f726372323707a
sourabh0396 <sourabhpatil0369@gmail.com>
2026-01-10 15:57:35 +0530

    added AI career suggestion and resume analyzer using Gemini

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,15 +4,16 @@ import uploadRotes from "./routes/uploadRotes.js"
 import { connectKafka } from './Producer.js';
 import userRoutes from "./routes/userRotes.js";
 import jobRotes from "./routes/jobRotes.js";
-
+import genAiRoutes from "./routes/genAiRoutes.js"
 const app = express();
 app.use(express.json())
 connectKafka();
 // app.use('/', (req, res) => {
 //     res.send('Home   Job Portal')
 // })
-app.use('/api/auth', authRotes)
-app.use('/api/upload', uploadRotes)
-app.use('/api/user', userRoutes)
-app.use('/api/job', jobRotes)
+app.use('/api/auth', authRotes);
+app.use('/api/upload', uploadRotes);
+app.use('/api/user', userRoutes);
+app.use('/api/job', jobRotes);
+app.use('/api/ai', genAiRoutes);
 export default app;
