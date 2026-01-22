e3189e6da274d460ca8cf6d3c1b130dfc198c9bb
sourabh0396 <sourabhpatil0369@gmail.com>
2026-01-22 08:51:45 +0530

    feat: add resume analyzer and about page UI with DialogContent integration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import CarrerGuide from "@/components/carrerguide";
 import Hero from "@/components/hero";
+import ResumeAnalyzer from "@/components/resume-analyser";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Hero />
       <CarrerGuide />
+      <ResumeAnalyzer />
     </div>
   );
 }
